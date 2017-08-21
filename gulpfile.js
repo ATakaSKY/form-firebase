7e0f800e3dbd4176cb383d89a1a6829a0c586de3
Aakash Thakur <thakurakash321@gmail.com>
2017-08-22 00:13:00 +0530

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    connect = require('gulp-connect'),
+    watch= require('gulp-watch'),
+    compass = require('gulp-compass'),
+    minifyCSS = require('gulp-minify-css');
+
+
+gulp.task('log',function() {
+    gutil.log('Workflows are awesome');
+})
+
+gulp.task('html',function(){
+    gulp.src('index.html')
+    .pipe(connect.reload()) 
+});
+
+gulp.task('compass', function() {
+  gulp.src('assets/sass/*.scss')
+    .pipe(compass({
+      sass: 'assets/sass'
+    }))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch',function(){
+    gulp.watch('index.html',['html']),
+    gulp.watch('assets/sass/*.scss',['compass']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+ 
+gulp.task('default', ['html','compass', 'connect', 'watch']);
